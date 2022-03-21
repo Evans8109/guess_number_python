89a02f1bb1fd248e1786394d2110858049036d89
Evans.chens <john113188@gmail.com>
2022-03-21 14:02:39 +0800

    version2

--- guess_game.py
+++ guess_game.py
@@ -1,17 +1,21 @@
-import random 
+import random
+start = input('please enter the start number: ')
+end = input('please enter the end number: ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 count = 0
 while True:
-	count += 1 # count = count + 1 
-	num = input('please enter your number: ')
-	num = int(num)
-	if r == num:
-		print('correct')
-		break
-	elif r > num:
-		print('more than', num, ) 
+        count += 1 # count = count + 1
+        num = input('please enter your number: ')
+        num = int(num)
+        if r == num:
+                print('correct')
+                break
+        elif r > num:
+                print('more than', num, )
 
-	elif r < num:
-		print('less than', num, )
-	print ('this is',  count, 'guess')
+        elif r < num:
+                print('less than', num, )
+        print ('this is',  count, 'guess')
